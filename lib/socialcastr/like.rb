0ffd1efa439475c56e514e01b4b9033a08628337
Riccardo Cambiassi <riccardo@headshift.com>
2011-05-06 22:41:16 +0100

    Like class needs an element_id

--- lib/socialcastr/like.rb
+++ lib/socialcastr/like.rb
@@ -1,9 +1,9 @@
 module Socialcastr
   class Like < Base
+    id_element
     element :unlikable
     element :user, :class => Socialcastr::User
     element :created_at
-    element :id
     def unlikable_by?(api_id)
       @unlikable && api_id == self.user_id ? true : false
     end
